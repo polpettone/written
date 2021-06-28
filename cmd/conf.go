35a6230f5a2f3effc95acf2573fcfc7c9f5ba156
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-06-28 13:58:10 +0200

    find and use pattern

--- cmd/conf.go
+++ cmd/conf.go
@@ -1,3 +1,4 @@
 package cmd
 
 const WrittenDirectory = "path_to_written_directory"
+const PatternsDirectory  = "path_to_patterns_directory"
--- cmd/new.go
+++ cmd/new.go
@@ -6,6 +6,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func NewCmd() *cobra.Command {
@@ -22,24 +24,68 @@ func NewCmd() *cobra.Command {
 	}
 }
 
-func findPattern(pattern string) {
-	fmt.Printf("Find pattern for name %s", pattern)
+type PatternFile struct {
+	info os.FileInfo
+	path string
+}
+
+func findPattern(pattern string) (string, error) {
+	fmt.Printf("Find pattern for name %s \n", pattern)
+	patternsDirectory := viper.GetString(PatternsDirectory)
+
+	var patternFiles []PatternFile
+
+	err := filepath.Walk(patternsDirectory,
+		func(path string, info os.FileInfo, err error) error {
+			patternFile := PatternFile{
+				info: info,
+				path: path,
+			}
+			patternFiles = append(patternFiles, patternFile)
+			return nil
+		})
+
+	if err != nil {
+		return "", err
+	}
+
+	var foundPatternFile *PatternFile
+	for _, file := range patternFiles {
+		if file.info.Name() == pattern {
+			foundPatternFile = &file
+			break
+		}
+	}
+
+	if foundPatternFile != nil {
+		patternContent, err := ioutil.ReadFile(foundPatternFile.path)
+		if err != nil {
+			return "", err
+		}
+		return string(patternContent), nil
+	}
+	return "", nil
 }
 
 
 func handleNewCommand(args []string, cobraCommand *cobra.Command) (string, error) {
-
-	patternName, _  := cobraCommand.Flags().GetString("pattern")
-    if patternName != "" {
-	    findPattern(patternName)
-    }
 
 	if len(args) != 1 {
 		return fmt.Sprintf("Please provide a name of the new written"), nil
 	}
 	name := args[0]
 
-	text, err := pkg.CaptureInputFromEditor("")
+	patternName, _  := cobraCommand.Flags().GetString("pattern")
+	var patternContent string
+    if patternName != "" {
+	    content, err := findPattern(patternName)
+	    if err != nil {
+	    	return "", err
+		}
+		patternContent = content
+    }
+
+	text, err := pkg.CaptureInputFromEditor(patternContent)
 	if err != nil {
 		return "", err
 	}
