65279c442efa81832aa8c901fdcf3436f1a6b946
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-10-17 22:38:18 +0200

    samples for ui

--- cmd/show.go
+++ cmd/show.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/polpettone/written/cmd/models"
 	"github.com/polpettone/written/cmd/service"
+	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -22,18 +24,85 @@ func ShowCmd() *cobra.Command {
 }
 
 func handleShowCommand() (string, error) {
+	simpleGrid()
+	return "", nil
+}
 
+func readDocuments() ([]models.Document, error) {
 	WrittenDirectory := viper.GetString(WrittenDirectory)
-	documents, err :=  service.Read(WrittenDirectory)
-
+	documents, err := service.Read(WrittenDirectory)
 	if err != nil {
-		return "", nil
+		return nil, err
 	}
-
-	return fmt.Sprintf("%v", documents), nil
+	return documents, nil
 }
 
 func init() {
 	showCmd := ShowCmd()
 	rootCmd.AddCommand(showCmd)
+}
+
+func simpleGrid() {
+
+	app := tview.NewApplication()
+
+	list := tview.NewList().
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+
+	documents, _ := readDocuments()
+
+	for _, document := range documents {
+		list.AddItem(document.Name, "", ' ' , nil)
+	}
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(text)
+	}
+	documentList := list
+
+	main := newPrimitive("Document Preview")
+
+	grid := tview.NewGrid().
+		SetRows(2, 0, 2).
+		SetColumns(30, 0).
+		SetBorders(true).
+		AddItem(newPrimitive("Header"), 0, 0, 1, 2, 0, 0, false).
+		AddItem(newPrimitive("Footer"), 2, 0, 1, 2, 0, 0, false)
+
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	grid.AddItem(documentList, 0, 0, 0, 0, 0, 0, false).
+		AddItem(main, 1, 0, 1, 2, 0, 0, false)
+
+	// Layout for screens wider than 100 cells.
+	grid.AddItem(documentList, 1, 0, 1, 1, 0, 100, false).
+		AddItem(main, 1, 1, 1, 1, 0, 100, false)
+
+	if err := app.SetRoot(grid, true).SetFocus(documentList).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func simpleListView() {
+	app := tview.NewApplication()
+	list := tview.NewList().
+		AddItem("List item 1", "Some explanatory text", 'a', nil).
+		AddItem("List item 2", "Some explanatory text", 'b', nil).
+		AddItem("List item 3", "Some explanatory text", 'c', nil).
+		AddItem("List item 4", "Some explanatory text", 'd', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+	if err := app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func simpleBox() {
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.15
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rivo/tview v0.0.0-20211001102648-5508f4b00266
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
