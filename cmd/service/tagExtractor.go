e918518b8a70c35c64eb8361656161395ce2f91e
Stefan Heiden <stefanheiden@gmail.com>
2021-11-13 21:40:02 +0100

    tag extractor

---
+++ cmd/service/tagExtractor.go
@@ -0,0 +1,17 @@
+package service
+
+import "strings"
+
+const TAG_PREFIX = "#"
+
+func extractTags(content string) []string {
+	fields := strings.Fields(content)
+
+	tags := []string{}
+	for _, f := range fields {
+		if len(f) > 1 && strings.HasPrefix(f, TAG_PREFIX) {
+			tags = append(tags, f)
+		}
+	}
+	return tags
+}
---
+++ cmd/service/tagExtractor_test.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_extractTags(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		content string
+		want    []string
+	}{
+
+		{
+			name:    "no tags",
+			content: "foo",
+			want:    []string{},
+		},
+
+		{
+			name:    "one tag",
+			content: "#foo",
+			want:    []string{"#foo"},
+		},
+
+		{
+			name:    "two tags",
+			content: "#tag0 #tag1",
+			want:    []string{"#tag0", "#tag1"},
+		},
+
+		{
+			name:    "no tag when markdown header",
+			content: "# header content",
+			want:    []string{},
+		},
+
+		{
+			name: "multi line with markdown header and one tag",
+			content: `
+			# header
+			content 
+			#tag
+			`,
+			want: []string{"#tag"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := extractTags(tt.content); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("extractTags() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
